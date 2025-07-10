b52abb01a78305182a640410c46ef1ec6b06ecf5
Dinka Dereje <dinqaadarajjee@gmail.com>
2025-07-10 10:19:45 +0300

    Dashboard Change

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,10 +2,13 @@ import {Card} from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue,fetchLatestInvoices,fetchCardData, fetchCustomers } from '../lib/data';
+import { Suspense } from 'react';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import { RevenueChartSkeleton,CardsSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { fetchLatestInvoices,fetchCardData } from '../../lib/data';
 export default async function Page() {
   try {
-    const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
     const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
 
@@ -13,18 +16,17 @@ export default async function Page() {
       <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          <Card title="Collected" value={totalPaidInvoices} type="collected" />
-          <Card title="Pending" value={totalPendingInvoices} type="pending" />
-          <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-          <Card
-            title="Total Customers"
-            value={numberOfCustomers}
-            type="customers"
-          />
+          <Suspense fallback={<CardsSkeleton />}>
+            <CardWrapper />
+          </Suspense>
         </div>
-        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-6">
-          <RevenueChart revenue={revenue} />
-          <LatestInvoices latestInvoices={latestInvoices} />
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+          <Suspense fallback={<RevenueChartSkeleton />}>
+            <RevenueChart />
+          </Suspense>
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <LatestInvoices />
+          </Suspense>
         </div>
       </main>
     );
